4990f27164cfec4cd7c0ded85f99799025bb9dbb
sannaide <53266447+sannaide@users.noreply.github.com>
2023-01-29 16:08:32 +0100

    Add files via upload

---
+++ components/mainmenu/header/mainmenu.php
@@ -0,0 +1,34 @@
+<div style="overflow:hidden;" class="topnav" id="myTopnav">
+  <div id="menuitems" style="float:left; overflow:hidden;">
+    <a id="menuitem1" href="https://sannaidelavoro.altervista.org/" class="active">Home</a>
+   	<a href="https://sannaidelavoro.altervista.org/pagina1/">Pagina 1</a>
+	<a href="https://sannaidelavoro.altervista.org/pagina2/">Pagina 2</a>
+	<a href="https://sannaidelavoro.altervista.org/pagina3/">Pagina 3</a>
+	<a href="https://sannaidelavoro.altervista.org/pagina4/">Pagina 4</a>
+	<a href="https://sannaidelavoro.altervista.org/articoli/">Articoli</a>
+	<a href="#">Prova</a>
+  </div>
+  <div style="float:right;">
+	<a href="javascript:void(0);" class="icon" onclick="toggleMenu()">
+		<i class="fa fa-bars"></i>
+	</a>
+  </div>
+  <div style="clear:both;"></div>
+</div>
+<div class="topnav-clone">
+  <div id="menuitems2" style="float:left;">
+    <a href="https://sannaidelavoro.altervista.org/" class="active">Home</a>
+   	<a href="https://sannaidelavoro.altervista.org/pagina1/">Pagina 1</a>
+	<a href="https://sannaidelavoro.altervista.org/pagina2/">Pagina 2</a>
+	<a href="https://sannaidelavoro.altervista.org/pagina3/">Pagina 3</a>
+	<a href="https://sannaidelavoro.altervista.org/pagina4/">Pagina 4</a>
+	<a href="https://sannaidelavoro.altervista.org/articoli/">Articoli</a>
+	<a href="#">Prova</a>
+  </div>
+  <div style="float:right;">
+	<a href="javascript:void(0);" class="icon" onclick="toggleMenu()">
+		<i class="fa fa-bars"></i>
+	</a>
+  </div>
+  <div style="clear:both;"></div>
+</div>
