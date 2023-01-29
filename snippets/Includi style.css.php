81291267ddf1ff39c574994b6a94e4156b7cf910
sannaide <53266447+sannaide@users.noreply.github.com>
2023-01-29 15:52:45 +0100

    Delete Includi style.css.php

--- snippets/Includi style.css.php
+++
@@ -1,6 +0,0 @@
-function custom_theme_assets() {
-	//wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css'));
-}
-
-add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
